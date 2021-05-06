7064475b62bddf7bbd3817d24693d1f2c8505bcc
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:16:32 -0700

    Corrects underpin autoloader

--- options.php
+++ options.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Option.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Options.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Option_Instance.php' );
-	Underpin\underpin()->get( $file )->loaders()->add( 'options', [
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'options', [
 		'registry' => 'Underpin_Options\Loaders\Options',
 	] );
-} );
+}, 10, 2 );
