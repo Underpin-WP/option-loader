b9e66e11264162827ff7dce3c0a8992932497304
Alex Standiford <a@alexstandiford.com>
2021-08-25 17:24:29 -0600

    Moves files into separate directories

--- lib/abstracts/Option.php
+++ lib/abstracts/Option.php
@@ -123,7 +123,7 @@ abstract class Option {
 		if ( isset( $this->$key ) ) {
 			return $this->$key;
 		} else {
-			return new WP_Error( 'batch_task_param_not_set', 'The batch task key ' . $key . ' could not be found.' );
+			return new WP_Error( 'post_template_param_not_set', 'The batch task key ' . $key . ' could not be found.' );
 		}
 	}
 
--- options.php
+++ options.php
@@ -5,9 +5,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 add_action( 'underpin/before_setup', function ( $file, $class ) {
-	require_once( plugin_dir_path( __FILE__ ) . 'Option.php' );
-	require_once( plugin_dir_path( __FILE__ ) . 'Options.php' );
-	require_once( plugin_dir_path( __FILE__ ) . 'Option_Instance.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Option.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Options.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Option_Instance.php' );
 	Underpin\underpin()->get( $file, $class )->loaders()->add( 'options', [
 		'registry' => 'Underpin_Options\Loaders\Options',
 	] );
